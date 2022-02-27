383b2ef60935e73f35de464bb8df9489353de8a1
macbook <khaitruong1301@gmail.com>
2022-02-27 09:49:15 +0700

    devA_commit_monHoc

---
+++ models/MonHoc.js
@@ -0,0 +1,7 @@
+function MonHoc() {
+    this.maMonHoc = '';
+    this.tenMonHoc = '';
+    this.hienThiThongTinMonHoc = function () {
+        console.log('Thông tin môn học', this.maMonHoc,this.tenMonHoc);
+    }
+}
