539c9fe91ef4e44940f6eaf4f7f5a5b694462280
macbook <khaitruong1301@gmail.com>
2022-02-27 09:44:03 +0700

    devb_code_lophoc.js

---
+++ models/LopHoc.js
@@ -0,0 +1,7 @@
+function LopHoc (){
+    this.maLop = '';
+    this.tenLop ='';
+    this.hienThiThongTinLop = function () {
+        console.log('Thông tin lớp học',this.maLop,this.tenLop);
+    }
+}
